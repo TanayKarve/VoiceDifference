e331cbc477e9b0a9e86e45d75b99e2d88417c1be
Tanay Karve <tanaykarve@gmail.com>
2019-03-18 16:08:31 +0700

    Create audio-difference.py

---
+++ audio-difference.py
@@ -0,0 +1,35 @@
+from scipy.io.wavfile import read
+def calc_distances(sound_file):
+    min_val = 5000
+    fs, data = read(sound_file)
+    data_size = len(data)
+    focus_size = int(0.15 * fs)
+    
+    focuses = []
+    distances = []
+    idx = 0
+    
+    while idx < len(data):
+        if data[idx] > min_val:
+            mean_idx = idx + focus_size // 2
+            focuses.append(float(mean_idx) / data_size)
+            if len(focuses) > 1:
+                last_focus = focuses[-2]
+                actual_focus = focuses[-1]
+                distances.append(actual_focus - last_focus)
+            idx += focus_size
+        else:
+            idx += 1
+    return distances
+    
+def accept_test(pattern, test, min_error):
+    if len(pattern) > len(test):
+        return False
+    for i, dt in enumerate(pattern):
+        if not dt - test[i] < min_error:
+            return False
+    return True
+pattern = calc_distances(input("Enter first music file")
+test = calc_distances("Enter second music file")
+min_error = 0.1
+print accept_test(pattern, test, min_error)
